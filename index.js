a062ac0a354554e037ea40ec8ca0fd57a5574dac
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-11-01 14:34:30 +0000

    rps prac finished and commented out

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-let userName = prompt("Welcome to RPS, What is your name?");
-let player = prompt(`hello ${userName}, enter your move (R, P or S)`);
-let playerMove = player.toUpperCase();
-let moves = ["ROCK", "PAPER", "SCISSORS"];
-let playerScore = 0;
-let computerScore = 0;
+//ROCK PAPER AND SCISSORS
 
-if (playerMove === "R") {
-  playerMove = "ROCK";
-} else if (playerMove === "P") {
-  playerMove = "PAPER";
-} else if (playerMove === "S") {
-  playerMove = "SCISSORS";
-}
+// let userName = prompt("Welcome to RPS, What is your name?");
+// let player = prompt(`hello ${userName}, enter your move (R, P or S)`);
+// let playerMove = player.toUpperCase();
+// let moves = ["ROCK", "PAPER", "SCISSORS"];
+// let computerScore = 0;
+// let playerScore = 0;
 
-console.log(`Player: ${playerMove}`);
+// if (playerMove === "R") {
+//   playerMove = "ROCK";
+// } else if (playerMove === "P") {
+//   playerMove = "PAPER";
+// } else if (playerMove === "S") {
+//   playerMove = "SCISSORS";
+// }
 
-let computerMove = moves[Math.floor(Math.random() * 3)];
-console.log(`PC: ${computerMove}`);
-if (playerMove === computerMove) {
-  prompt(
-    `its a draw!!! you both chose ${playerMove}... The score is: \n Player:${playerScore} \n Computer: ${computerScore}`
-  );
-} else if (
-  (playerMove === "ROCK" && computerMove === "SCISSORS") ||
-  (playerMove === "PAPER" && computerMove === "ROCK") ||
-  (playerMove === "SCISSORS" && computerMove === "PAPER")
-) {
-  playerScore++;
-  prompt(
-    `you WIN!!! your ${playerMove} beats the computers ${computerMove}... The score is: \n Player:${playerScore} \n Computer: ${computerScore}`
-  );
-} else {
-  computerScore++;
-  prompt(
-    `you LOSE!!! the computers ${computerMove} beats your ${playerMove}... The score is: \n Player:${playerScore} \n Computer: ${computerScore}`
-  );
-}
-playAgain = prompt(
-  "would you like to play again? enter y to continue, or n to exit"
-).toLowerCase();
+// console.log(`Player: ${playerMove}`);
+
+// let computerMove = moves[Math.floor(Math.random() * 3)];
+// console.log(`PC: ${computerMove}`);
+// if (playerMove === computerMove) {
+//   prompt(
+//     `its a draw!!! you both chose ${playerMove}... The score is: \n Player:${playerScore} \n Computer: ${computerScore}`
+//   );
+// } else if (
+//   (playerMove === "ROCK" && computerMove === "SCISSORS") ||
+//   (playerMove === "PAPER" && computerMove === "ROCK") ||
+//   (playerMove === "SCISSORS" && computerMove === "PAPER")
+// ) {
+//   playerScore++;
+//   prompt(
+//     `you WIN!!! your ${playerMove} beats the computers ${computerMove}... The score is: \n Player:${playerScore} \n Computer: ${computerScore}`
+//   );
+// } else {
+//   computerScore++;
+//   prompt(
+//     `you LOSE!!! the computers ${computerMove} beats your ${playerMove}... The score is: \n Player:${playerScore} \n Computer: ${computerScore}`
+//   );
+// }
+// playAgain = prompt("to continue, enter y, or n to stop");
